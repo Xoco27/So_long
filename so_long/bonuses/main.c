d0e78e17fb7f5b414a86edf64017714c62ecf738
Corentin <cf.fleuret@gmail.com>
2024-12-13 16:34:01 +0100

    Save

---
+++ so_long/bonuses/main.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfleuret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 15:17:44 by cfleuret          #+#    #+#             */
+/*   Updated: 2024/12/12 14:46:44 by cfleuret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "so_long.h"
+
+void	set_win(t_data *data)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (data->map[y])
+		y++;
+	x = ft_strlen(data->map[0]);
+	data->win_height = y * TILE;
+	data->win_width = x * TILE;
+}
+
+int	on_destroy(t_data *data)
+{
+	destroy(data);
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+	free_map(data->map);
+	exit(0);
+	return (0);
+}
+
+int	on_keypress(int keysym, t_data *data)
+{
+	char	*move;
+
+	if (keysym == XK_s)
+		down_char(data->map, data);
+	if (keysym == XK_w)
+		up_char(data->map, data);
+	if (keysym == XK_d)
+	{
+		right_char(data->map, data);
+		if (data->map[data->player.pos_y][data->player.pos_x] == 'F')
+			on_destroy(data);
+	}
+	if (keysym == XK_a)
+		left_char(data->map, data);
+	if (keysym == XK_Escape)
+		on_destroy(data);
+	move = ft_itoa(data->move);
+	print_img(data->img.wall, 0, 0, data);
+	mlx_string_put(data->mlx_ptr, data->win_ptr, 10, 10, 0xFFFFFF, "Moves:");
+	mlx_string_put(data->mlx_ptr, data->win_ptr, 70, 10, 0xFFFFFF, move);
+	free(move);
+	ft_printf("move: %d\n", data->move);
+	if (data->score == 0
+		&& data->map[data->player.pos_y][data->player.pos_x] == 'E')
+	{
+		ft_printf("You won in %d moves !\n", data->move);
+		on_destroy(data);
+	}
+	return (0);
+}
+
+void	hook(t_data *data)
+{
+	mlx_hook(data->win_ptr, KeyRelease, KeyReleaseMask, on_keypress, data);
+	mlx_hook(data->win_ptr, DestroyNotify, StructureNotifyMask,
+		on_destroy, data);
+	mlx_loop(data->mlx_ptr);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+
+	if (argc != 2)
+		return (perror("Error\nNot enough or too much arguments."), 1);
+	data.move = 0;
+	data.map = make_tab(data.map, argv);
+	if (another_check(&data) == 1)
+		return (1);
+	if (!data.map)
+		return (perror("Error\nMap making failed."), 1);
+	set_win(&data);
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (perror("Error\nFailure initiating mlx."), 1);
+	data.win_ptr = mlx_new_window(data.mlx_ptr,
+			data.win_width, data.win_height, "So_long");
+	if (!data.win_ptr)
+		return (perror("Error\nFailure initiating window"), 1);
+	create_images(&data);
+	print_map(data.map, &data);
+	print_foe(&data);
+	pos(data.map, &data);
+	hook(&data);
+	free_map(data.map);
+	return (0);
+}
---
+++ so_long/functions/images.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   images.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfleuret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 13:43:30 by cfleuret          #+#    #+#             */
+/*   Updated: 2024/12/11 12:05:53 by cfleuret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "so_long.h"
+
+void	wh(t_data *data)
+{
+	data->img.width = TILE;
+	data->img.height = TILE;
+	data->player.width = TILE;
+	data->player.height = TILE;
+}
+
+void	create_images(t_data *data)
+{
+	wh(data);
+	data->img.wall = mlx_xpm_file_to_image(data->mlx_ptr,
+			"assets/wall.xpm", &data->img.width, &data->img.height);
+	data->img.floor = mlx_xpm_file_to_image(data->mlx_ptr,
+			"assets/floor.xpm", &data->img.width, &data->img.height);
+	data->img.exit_closed = mlx_xpm_file_to_image(data->mlx_ptr,
+			"assets/door_closed.xpm", &data->img.width, &data->img.height);
+	data->img.exit_opened = mlx_xpm_file_to_image(data->mlx_ptr,
+			"assets/door_opened.xpm", &data->img.width, &data->img.height);
+	data->img.item = mlx_xpm_file_to_image(data->mlx_ptr,
+			"assets/item.xpm", &data->img.width, &data->img.height);
+	data->player.up = mlx_xpm_file_to_image(data->mlx_ptr,
+			"assets/link_up.xpm", &data->player.width, &data->player.height);
+	data->player.down = mlx_xpm_file_to_image(data->mlx_ptr,
+			"assets/link_down.xpm", &data->player.width, &data->player.height);
+	data->player.right = mlx_xpm_file_to_image(data->mlx_ptr,
+			"assets/link_right.xpm", &data->player.width, &data->player.height);
+	data->player.left = mlx_xpm_file_to_image(data->mlx_ptr,
+			"assets/link_left.xpm", &data->player.width, &data->player.height);
+}
+
+void	destroy(t_data *data)
+{
+	mlx_destroy_image(data->mlx_ptr, data->img.wall);
+	mlx_destroy_image(data->mlx_ptr, data->img.floor);
+	mlx_destroy_image(data->mlx_ptr, data->img.exit_closed);
+	mlx_destroy_image(data->mlx_ptr, data->img.exit_opened);
+	mlx_destroy_image(data->mlx_ptr, data->img.item);
+	mlx_destroy_image(data->mlx_ptr, data->player.up);
+	mlx_destroy_image(data->mlx_ptr, data->player.down);
+	mlx_destroy_image(data->mlx_ptr, data->player.right);
+	mlx_destroy_image(data->mlx_ptr, data->player.left);
+}
